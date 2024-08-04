277158a4d7ff19e9279ff79153aae18a0f85b89a
Chinmay Harishchandra Mondkar <125800575+chinu2003@users.noreply.github.com>
2024-08-04 22:33:44 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-alert("hell0")
+const fs = require("fs");
+
+
+// write file in node js  
+// fs.writeFile("hey.txt", "hello world", (err) => {
+//   if (err) console.error(err);
+//   else console.log("done");
+// });
+
+
+
+// append in node js 
+// fs.appendFile("hello.txt", " good morning", (err) => {
+//   if (err) console.error(err);
+//   else console.log("done");
+// });
+
+// rename in node js 
+// fs.rename("hey.txt", "hello.txt", (err) => {
+//   if (err) console.error(err);
+//   else console.log("done");
+// });
+
+// copy in node js 
+// fs.copyFile("hello.txt", "copy.txt", (err) => {
+//   if (err) console.error(err);
+//   else console.log("done");
+// });
+
+// remove in node js 
+// fs.unlink("copy.txt", (err) => {
+//   if (err) console.error(err);
+//   else console.log("done");
+// });
+
+
+// to read file in node js 
+
+// fs.readFile('./hello.txt', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   }); 
